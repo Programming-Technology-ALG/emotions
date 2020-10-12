df0d62bf6433a97f144cf0471d4c8a1ac9602c7b
alexgiving <alexgiving@mail.ru>
2020-10-13 00:36:55 +0300

    firs and last

---
+++ main.py
@@ -0,0 +1,27 @@
+def check_world(i: int, corrected: str):
+    global string
+    global starts
+    if i >= len(starts) - 1:
+        if corrected in dic:
+            string = corrected
+        return string
+    if starts[i + 1] - starts[i] > 1:
+        check_world(i + 1, corrected + str(w[starts[i]]) + str(w[starts[i]]))
+    check_world(i + 1, corrected + str(w[starts[i]]))
+    return string
+
+
+with open('words.txt', "r") as f:
+    dic = set(f.read().split())
+    print("Program starts. Type \exit to close.")
+    while True:
+        string = ""
+        temp = str(input("Input word: "))
+        if temp == "\exit":
+            break
+        w = "  " + temp + "  "
+        starts = [i for i, c in list(enumerate(w))[2:] if c != w[i - 1]]
+        if check_world(0, "") == "":
+            print("There is no such word as", w, "\n")
+        else:
+            print("There is similar word as", string, "\n")
